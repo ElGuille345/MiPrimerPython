24d4e0649ea9ed871a70cd129f9be11af6239532
ElGuille345 <91351784+ElGuille345@users.noreply.github.com>
2021-09-24 21:12:46 -0300

    Add files via upload

---
+++ programa1.py
@@ -0,0 +1,14 @@
+print ("---------------------------------------")
+print("crédito Guillermo")
+print ("---------------------------------------")
+
+
+varNombre = input ("Ingrese su nombre: ")
+varApellido = input ("Ingrese su apellido: ")
+varEdad = input ("Ingrese su edad: ") 
+varMonto = input ("Ingrese el monto de su crédito: ") 
+varSalario = input ("Ingrese su sueldo mensual: ")
+if varMonto > varSalario:
+    print("El crédito supera su sueldo: ") 
+if varMonto < varSalario:
+    print("Se procede a depositar dinero a la brevedad")    
